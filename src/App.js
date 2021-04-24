245ca0af895c3b528b2224d36fa113fc9dda9402
graypidgeon <whitekangur@wp.pl>
2021-04-24 15:27:25 +0200

    Zmiana

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. dupa
+		wprowadzono zmiane
         </p>
         <a
           className="App-link"
